0c98c730724585f3a6894b96885232df407d8770
Christoph Ortner <c.ortner@warwick.ac.uk>
2021-04-14 15:20:13 -0700

    more updatres L5

--- ata_05_highdim.jl
+++ ata_05_highdim.jl
@@ -4,12 +4,14 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 3f1cfd12-7b86-11eb-1371-c5795b87ef5b
-begin
-	using Plots, LaTeXStrings, PrettyTables, DataFrames, LinearAlgebra,
-		  PlutoUI, BenchmarkTools, ForwardDiff, Printf, FFTW
-	include("tools.jl")
-end;
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
 
 # ╔═╡ 76e9a7f6-86a6-11eb-2741-6b8759be971b
 md"""
@@ -643,12 +645,232 @@ md"""
 md"""
 ## §5.3 Approximation in High Dimension
 
+Let us return to the approximation of an analytic function ``f \in C_{\rm per}(\mathbb{R}^d)``. We have proven above that 
+```math
+	\|f - I_N f \|_\infty \lesssim (\log N)^d e^{- \alpha N}
+```
+for some ``\alpha > 0``, which appears to be an excellent generalisation of our 1D results. However, this is deceptive. The number of parameters required to define or to evaluate ``I_N f`` is ``O(N^d)``, and the work to construct it is ``O( (N \log N)^d)``. This cost is **considerable** when ``d`` is large. For example, consider a function in just 10 dimensions, and discretise with just 10 parameters in each coordinate direction (``N = 5``), then we obtain already ``10^10``, or, 10 billion, parameters. Going into slightly higher dimension, say ``d = 100``, we require ``10^100`` parameters. The number of atoms in the universe is estimated to be around ``10^{78}`` to ``10^{82}``. This extreme explosion of computational cost with high dimension is called the [**CURSE OF DIMENSIONALITY**](https://en.wikipedia.org/wiki/Curse_of_dimensionality).
+
+
+If we want to express this in terms of a error-cost relationship, we define a new convergence parameter ``{\rm COST}`` and rewrite our error estimate as 
+```math
+	\|f - I_N f \|_\infty \lesssim d^{-d} (\log {\rm COST})^d e^{- \alpha {\rm COST}^{1/d}}
+```
+We can equally see how this rate deteriorates rapidly with increasing ``d``. 
+
+How is approximation in high dimension then possible? The main point is that it is only possible if we can exploit specific features of the target function ``f``. For example, in the next lecture I will introduce approximation problems in dimension ``d \approx 100``, provided it satisfies some very strong symmetries.
+"""
+
+# ╔═╡ 228aba30-9cd9-11eb-0bdc-e7b4237757ce
+md"""
+### A Greedy Algorithm
+
+A simple way --- at least conceptually --- to try and find "sparsity" in the high-dimensional function is via Fourier analysis. Suppose we have the Fourier series of ``f``, i.e., 
+```math
+	f({\bf x}) = \sum_{{\bf k} \in \mathbb{Z}^d} \hat{f}_{\bf k} e^{i {\bf k} \cdot {\bf x}}
+```
+then we can ask what the effect would be of projecting ``f`` not onto the "canonical" space
+```math
+	{\rm span} \big\{ e^{i {\bf k} \cdot {\bf x}} \,|\, {\bf k} \in \{-N,\dots, N\}^d \big\},
+```
+but onto an *arbitrary* subset of the full basis. I.e., we first choose a finite set  
+```math
+	\mathcal{K} \subset \mathbb{Z}^d
+```
+and then define the corresponding ``L^2``-projection 
+```math
+	t_{\mathcal{K}} := \sum_{{\bf k} \in \mathcal{K}}  \hat{f}_{\bf k} e^{i {\bf k} \cdot {\bf x}}.
+```
+Since the multi-variate trigonometric polynomials are orthonormal and bounded, the error in the ``L^2`` and max-norms will be [ASSIGNMENT]
+```math
+	\begin{aligned}
+		\| f - t_{\mathcal{K}} \|_{L^2( (0, 2\pi)^2 )} 
+			&= \bigg( \sum_{{\bf k} \in \mathbb{Z}^d \setminus \mathcal{K}} 
+						|\hat{f}_k|^2 \bigg)^{1/2}, \qquad \text{and} \\ 
+		\| f - t_{\mathcal{K}} \|_{\infty}
+			&\leq \sum_{{\bf k} \in \mathbb{Z}^d \setminus \mathcal{K}}  |\hat{f}_k|.
+	\end{aligned}
+```
+Thus, we see that to get the best ``M``-term approximation in the ``L^2`` sense, or the best possible upper bound as derived above in the max-norm, we should fill ``mathcal{K}`` with the indices ``{\bf k}`` corresponding to the ``M`` largest Fourier coefficients. 
+
+This leads to the following, purely hypothetical, "greedy" algorithm: 
+
+**Greedy Best Approximation Algorithm:**
+* Compute all fourier coefficients ``\hat{f}_{\bf k}``
+* Sort then in descenting order: ``{\bf k}_1, {\bf k}_2, \dots`` such that ``|\hat{f}_{{\bf k}_m}| \geq |\hat{f}_{{\bf k}_{m+1}}|``.
+* Define ``\mathcal{K}_M := \{{\bf k}_1, \dots, {\bf k}_M\}`` and the resulting best ``M``-term approx ``t_{\mathcal{K}_M}``.
+
+**Remarks:**
+* The greedy algorithm produces the best ``M``-term approximation in the ``L^2``-sense but it is by no means clear that it gives the best ``M``-term approximation in the max-norm (or indeed quasi-best...) 
+* Since the basis functions are most efficiently computed via a recursion it is not even clear that the best ``M``-term approximation gives also the best cost error ratio. 
+* It is not always clear how to select "the most important" Fourier coefficients in practise.
+
+[[DeVore, 1997]](https://www.cambridge.org/core/journals/acta-numerica/article/abs/nonlinear-approximation/C8E028C39B8A849690D0EC418516A934)
+"""
+
+# ╔═╡ 1e27020a-9cd9-11eb-2b27-8956a221427c
+md"""
+### Mixed Regularity and Sparse Grids
+
+Because it can be difficult to implement a best ``M``-term approximation in practise we are also interested in how general function classes lead to generically good choices of the sparse basis set ``\mathcal{K}``. 
+
+A very general and broadly applicable idea is to exploit the existence of **mixed derivatives.**
+
+For example, suppose that ``f \in C_{\rm per}`` and that the derivatives 
+```math
+	f, \partial_{x_1} f, \partial_{x_2} f,  \partial_{x_1} \partial_{x_2} f \in L^2
+```
+then it is straightforward to show that [LN, Sec. 8.4] and **[WHITEBOARD]**
+```math
+	\sum_{{\bf k} \in \mathbb{Z}^2} 
+	(1 + |k_1|)^2 (1 + |k_2|)^2 |\hat{f}_{\bf k}|^2 
+	\leq 
+	\|f\|_2^2 + \| \partial_{x_1} f \|_2^2 + \|\partial_{x_2} f\|_2^2 
+	+ \| \partial_{x_1} \partial_{x_2} f \|_2^2 < \infty.
+```
+"""
+
+# ╔═╡ 80cf2b4a-9d55-11eb-2908-8f19c79bca96
+md"""
+This gives us a rough estimate on how the Fourier coefficients must be decaying, i.e. it suggests that they might decay like some function of ``(1 + |k_1|)^{-1} (1 + |k_2|)^{-1}``. The idea now is to use this "characteristic decay" to produce the sparse basis ``\mathcal{K}`` instead of the Fourier coefficients themselves. That is, we can choose 
+```math
+	\mathcal{K}_N := \big\{ {\bf k} : (1 + |k_1|) (1 + |k_2|) \leq N \big\}.
+```
+This is a variant of the *hyperbolic cross approximation*. Named after the level sets of the characteristic decay function ``\omega({\bf k}) = (1 + |k_1|) (1 + |k_2|)``.
+"""
+
+# ╔═╡ abddcd1e-9d55-11eb-158a-098df3e34ffe
+let N = 10, ω = kk -> (1+abs(kk[1])) * (1+ abs(kk[2]))
+	k = -N:N
+	Kx = k * ones(2N+1)'; Ky = (Kx')[:]; Kx = Kx[:]
+	Ihc = findall(ω.([ [kx, ky] for (kx, ky) in zip(Kx, Ky) ]) .<= N+1)
+	scatter(Kx, Ky, ms=3, label = "Tensor Grid")
+	scatter!(Kx[Ihc], Ky[Ihc], ms=3, label = "Hyperbolic Cross", 
+				size = (430,250), legend = :outertopright, 
+				xlabel = L"k_1", ylabel = L"k_2")
+end
+
+# ╔═╡ a59bcffe-9d56-11eb-10d5-8da3f1e7a7ea
+md"""
+More generally, one can try to classify functions in high dimension in terms of *Korobov classes*: given a weight ``\omega : \mathbb{Z}^d \to (0, 1]`` we consider the functions
+```math
+	\begin{aligned}
+	\mathcal{A}_{\omega}^{(2)} 
+	&:= \bigg\{ f : \sum_{{\bf k} \in \mathbb{Z}^d} \omega({\bf k })^2 |\hat{f}_{\bf k}|^2 < \infty \bigg\} \\ 
+	\mathcal{A}_{\omega}^{(\infty)} 
+	&:= \bigg\{ f : \sum_{{\bf k} \in \mathbb{Z}^d} \omega({\bf k }) |\hat{f}_{\bf k}| < \infty \bigg\} 
+\end{aligned}
+```
+
+Suppose e.g. that ``f \in \mathcal{A}^{(p)}_\omega`` then a natural way to choose ``\mathcal{K}`` would be 
+```math
+	\mathcal{K}_\epsilon := \big\{ {\bf k} : \omega({\bf k}) \leq \epsilon^{-1} \big\}, 
+```
+and this would lead to the error bound **[WHITEBOARD]**
+```math
+	\| f - t_{\mathcal{K}_\epsilon} \|_p \leq \epsilon.
+```
+
+What remains to be done now is to understand the computational cost associated with these *sparse approximations*, or maybe somewhat easier, what is the size of the basis set --- e.g. ``\mathcal{K}_\epsilon`` above --- that we selected? For that we return to the hyperbolic cross example but generalize it in various ways.
+"""
+
+# ╔═╡ 4e8fd706-9d5a-11eb-392d-5fb9f8ba4b30
+md"""
+### Hyperbolic cross in ``d`` dimensions
 
 
 """
 
+# ╔═╡ 8f7d968e-9d61-11eb-2cbc-47918f263af2
+begin
+	function imcoeffs(C; logscale=true)
+		if logscale 
+			img = log.(abs.(C) .+ 1e-15)
+		else
+			img = abs.(C)
+		end
+		a, b = extrema(img)
+		img = (img .- a) ./ (b - a)
+		return Gray.(img)		
+	end
+end
+
+# ╔═╡ 148992ca-9d66-11eb-3787-b3db6ad34765
+md"""
+### Some sparsity patterns ... 
+
+To understand "real-life" sparsity patterns we will look at a few 2D examples. These are of course extremely limited and in no way reflect the complexity of proper high-dimensional approximation.
+
+First a "random" analytic function 
+```math
+f(x_1, x_2) = \exp\Big( \sin(2 x_1) \cos( \sin(x_2)) \Big)
+```
+In the following image we plot the magnitudes of the Fourier coefficients, with the ``x``-axis representing ``k_1`` and the ``y``-axis ``k_1``. It is important to remember here the ordering of the Fourier coefficients, ``(0, 1, \dots, N, -N+1, -N+2, \dots, -1)`` i.e. the four corners represent the small ``{\bf k}`` while the center represents the large ``{\bf k}``.
+"""
+
+# ╔═╡ fec961f6-9d5e-11eb-3263-ed551a9e753f
+let f = (x1, x2) -> exp(sin(2*x1)*cos(sin(x2))), N = 32
+	F̂ = triginterp2d(f, N)
+	imcoeffs(F̂)
+end
+
+# ╔═╡ eb4f6e28-9d5d-11eb-14af-350b86a80a62
+md"""
+A multi-dimensional witch of Agnesi, 
+```math
+	f({\bf x}) = \frac{1}{1 + c |\sin.({\bf x})|^2}
+```
+"""
+
+# ╔═╡ 8af26c20-9d61-11eb-01dc-9d4af89e0459
+let f = (x1, x2) -> 1 / (1 + 50*(sin(x1)^2 + sin(x2)^2)), N = 256
+	F̂ = triginterp2d(f, N)
+	imcoeffs(F̂[1:2:end, 1:2:end])
+end
+
+# ╔═╡ 771181e8-9d5f-11eb-05fb-83a91ead7fa0
+md"""
+Secondly, an examples taken from electron [transport models](https://arxiv.org/pdf/1907.01314.pdf) (for this function we would again want to evaluate it as a bi-variate matrix function!)
+```math
+	f(x_1, x_2) = \frac{f_\beta(x_1) - f_\beta(x_2)}{x_1 - x_2} \cdot \frac{1}{x_1 - x_2 + i \eta},
+```
+where ``f_\beta`` is the Fermi-Dirac function and ``\eta`` a physical parameter (relaxation time of a wave) which is small. The prefactor ``\frac{f_\beta(x_1) - f_\beta(x_2)}{x_1 - x_2}`` is in fact analytic (a bit of an exercise) so we focus on the last term ``\frac{1}{x_1 - x_2 + i \eta}``. 
+
+``\eta = `` $(@bind __eta Slider(0.01:0.01:0.3, show_value=true))
+"""
+
+# ``N = `` $(@bind __N Slider(32:32:512, show_value=true))
+
+
+
+# ╔═╡ 2d580e02-9d62-11eb-282d-394a4745ffff
+let η = __eta, N = 128, f = (x1, x2) -> 1 / (sin(x2) - sin(x1) + im * η)
+	F̂ = triginterp2d(f, N)
+	imcoeffs(F̂; logscale=true)
+end
+
+# ╔═╡ 27c4b85c-9cd9-11eb-2f0d-71d2ca0913f9
+md"""
+### Outlook
+
+In this last lecture I was focusing mostly on theory. Getting high-dimensional approximation to work in practise, for "real" problems is computationally much more challenging. Many further ideas might be needed, e.g. next week we will talk about a sparse ACE approximation which involves reductions due to: sparse grids, symmetrized basis, recursive evaluation schemes, body-order expansion, to name just a few.
+
+The relatively simple ideas outlined above should only be taken as motivation for further reading. Most work in this domain is necessarily application specific but the overarching idea is that "real-world" signals however high-dimensional should be representable in some sparse format. But exactly what these formats are can vary greatly across different applications. Automagically discovering these sparsities is one of the goals of artificial neural networks (success is mixed...). Analysis and modelling can do a lot to help!
+"""
+
+# ╔═╡ 72f978e8-9d61-11eb-13ef-4ff5f4ef690a
+using Colors
+
+# ╔═╡ 3f1cfd12-7b86-11eb-1371-c5795b87ef5b
+begin
+	using Plots, LaTeXStrings, PrettyTables, DataFrames, LinearAlgebra,
+		  PlutoUI, BenchmarkTools, ForwardDiff, Printf, FFTW, Colors
+	include("tools.jl")
+end;
+
 # ╔═╡ Cell order:
-# ╟─3f1cfd12-7b86-11eb-1371-c5795b87ef5b
+# ╠═3f1cfd12-7b86-11eb-1371-c5795b87ef5b
+# ╠═72f978e8-9d61-11eb-13ef-4ff5f4ef690a
 # ╟─76e9a7f6-86a6-11eb-2741-6b8759be971b
 # ╟─551a3a58-9b4a-11eb-2596-6dcc2edd334b
 # ╟─a7e7ece4-9b48-11eb-26a8-213556563a81
@@ -685,4 +907,18 @@ md"""
 # ╟─704b6176-9c15-11eb-11ba-ffb4491a6003
 # ╠═fd07f1d2-9c89-11eb-023f-e96ccdd43a7e
 # ╠═7489004a-9c15-11eb-201d-91f34cb40c6f
-# ╠═e47464b6-9bd2-11eb-2404-6b4a6459ee31
+# ╟─e47464b6-9bd2-11eb-2404-6b4a6459ee31
+# ╟─228aba30-9cd9-11eb-0bdc-e7b4237757ce
+# ╟─1e27020a-9cd9-11eb-2b27-8956a221427c
+# ╟─80cf2b4a-9d55-11eb-2908-8f19c79bca96
+# ╟─abddcd1e-9d55-11eb-158a-098df3e34ffe
+# ╟─a59bcffe-9d56-11eb-10d5-8da3f1e7a7ea
+# ╠═4e8fd706-9d5a-11eb-392d-5fb9f8ba4b30
+# ╟─8f7d968e-9d61-11eb-2cbc-47918f263af2
+# ╟─148992ca-9d66-11eb-3787-b3db6ad34765
+# ╟─fec961f6-9d5e-11eb-3263-ed551a9e753f
+# ╟─eb4f6e28-9d5d-11eb-14af-350b86a80a62
+# ╟─8af26c20-9d61-11eb-01dc-9d4af89e0459
+# ╟─771181e8-9d5f-11eb-05fb-83a91ead7fa0
+# ╟─2d580e02-9d62-11eb-282d-394a4745ffff
+# ╟─27c4b85c-9cd9-11eb-2f0d-71d2ca0913f9
